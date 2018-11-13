bde6da9055d8dfac0a2eabd70a7bd1fb5f7159a6
Tom Noonan II <tom@tjnii.com>
2018-11-12 21:29:02 -0500

    Speed up tests

--- app/spec/backup_engine_functional_spec.rb
+++ app/spec/backup_engine_functional_spec.rb
@@ -13,8 +13,6 @@ require 'logger'
 describe 'Backup Engine: Functional' do
   target_paths = Pathname.new('/').children.reject { |path| %w[/proc /sys /dev /tmp].include? path.to_s }.freeze
 
-  let(:restore_path) {  Pathname.new('/tmp/test_restore') }
-
   before :all do
     @communicator = BackupEngine::Communicator.new(type: "filesystem", backend_config: {base_path: '/tmp/test_backup_output'})
     @checksum_engine = BackupEngine::Checksums::Engine.new("sha256")
@@ -44,30 +42,62 @@ describe 'Backup Engine: Functional' do
   it 'restores without errors' do
     restore_engine = BackupEngine::RestoreClient::Engine.new(communicator: @communicator, logger: @logger)
     restore_engine.restore_manifest(manifest_path: @backup_engine.manifest.path,
-                                    restore_path: restore_path)
+                                    restore_path: Pathname.new('/tmp/test_restore'))
   end
 
   target_paths.each do |path|
     # Intentionally not using Ruby File methods in order to test differently than the implementation,
-    # and because of odd behavior like File.stat() always following symlinks.
-    files = `find #{path}`.split.map { |file| file.sub(%r{^/}, '') }
-    files.each do |file|
-      it "restores proper types and permissions on #{file}" do
-        output = [restore_path, '/'].map do |tgt_path|
-          # Ignore times, link count, sizes (Mismatch on directories)
-          `cd #{tgt_path}; ls -nd --time-style=+ "#{file}"`.split.select.with_index { |_, i| [0, 2, 3].include? i }
-        end
-        expect(output[0]).to eql(output[1])
-      end
-    end
 
-    files = `find #{path} -type f`.split.map { |file| file.sub(%r{^/}, '') }
-    files.each do |file|
-      it "restores proper file contents on #{file}" do
-        output = [restore_path, '/'].map do |tgt_path|
-          `cd #{tgt_path}; cat "#{file}" | wc -c; sha512sum "#{file}"`
+    describe "#{path} permissions" do
+      files = `find #{path}`.split("\n").map { |file| file.sub(%r{^/}, '') }
+      before :all do
+        # Stat all the files in one shell to avoid the cost of a subshell for each file
+        files_fd = Tempfile.new
+        files_fd.write(files.join("\n"))
+        files_fd.close
+
+        @perm_output = Hash.new { |h,k| h[k] = Hash.new }.tap do |hash|
+          {src: '/tmp/test_restore', dst: '/'}.each_pair do |key, tgt_path|
+            raw = `cd #{tgt_path}; set -e; cat #{files_fd.path} | while read file; do ls -nd --time-style=+ "${file}"; done`
+            raise("Failed to gather #{key} stats in #{tgt_path}") if $? != 0
+            raw.split("\n").each do |line|
+              parsed_line = line.split.select.with_index { |_, i| [0, 2, 3, 5].include? i }
+              hash[parsed_line.delete_at(3)][key] = parsed_line
+            end
+          end
         end
-        expect(output[0]).to eql(output[1])
+      end
+              
+      files.each do |file|
+        it "are correct for #{file}" do
+          expect(@perm_output[file][:dst]).to eql(@perm_output[file][:src])
+        end
+      end
+
+      describe "#{path} contents" do
+        files = `find #{path} -type f`.split("\n").map { |file| file.sub(%r{^/}, '') }
+        before :all do
+          # Stat all the files in one shell to avoid the cost of a subshell for each file
+          files_fd = Tempfile.new
+          files_fd.write(files.join("\n"))
+          files_fd.close
+
+          @cont_output = Hash.new { |h,k| h[k] = Hash.new }.tap do |hash|
+            {src: '/tmp/test_restore', dst: '/'}.each_pair do |key, tgt_path|
+              raw = `cd #{tgt_path}; set -e; cat #{files_fd.path} | while read file; do echo "$(ls "${file}" -d):$(cat "${file}" | wc -c):$(sha512sum "${file}")"; done`
+              raise("Failed to check #{key} contents #{tgt_path}") if $? != 0
+              raw.split("\n").map { |line| line.split(':') }.each do |line|
+                hash[line.delete_at(0)][key] = line
+              end
+            end
+          end
+        end
+
+        files.each do |file|
+          it "are the same for #{file}" do
+            expect(@cont_output[file][:dst]).to eql(@cont_output[file][:src])
+          end
+        end
       end
     end
   end
