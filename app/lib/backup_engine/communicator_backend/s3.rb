ed62a0380316ccf6e7801b862ee602a4f68a4c4e
Tom Noonan II <tom@tjnii.com>
2019-01-13 23:15:40 -0500

    S3 support

--- app/lib/backup_engine/communicator_backend/s3.rb
+++ app/lib/backup_engine/communicator_backend/s3.rb
@@ -1,15 +1,107 @@
-require 'pathname'
-require 'fileutils'
+require 'aws-sdk-s3'
+
+require_relative '../pathname.rb'
+require_relative 's3_list_cache.rb'
 
 module BackupEngine
   module CommunicatorBackend
+    class S3CommunicatorError < StandardError
+    end
+
     class S3
-      # WIP
-      # rubocop: disable Lint/UnusedMethodArgument
-      def initialize(bucket:)
-        raise('STUBBED')
+      def initialize(bucket:, s3_client_config:, storage_class: 'STANDARD_IA')
+        @bucket = bucket.freeze
+        @storage_class = storage_class.freeze
+
+        # Do not enforce any required keys as they can all come form ENV vars, AWS config files, etc...
+        if s3_client_config.key?('credentials')
+          # Reformat credentials into the proper object, because AWS
+          s3_client_config['credentials'] = Aws::Credentials.new(s3_client_config['credentials'].fetch('access_key_id'), s3_client_config['credentials'].fetch('secret_access_key'))
+        end
+        @s3 = Aws::S3::Client.new(s3_client_config.symbolize_keys)
+
+        @cache = S3ListCache.new(initial_date: Time.new(0))
       end
-      # rubocop: enable Lint/UnusedMethodArgument
+
+      def date(path:)
+        _s3_list(path: path) unless @cache.exists?(path: path)
+        @cache[path]
+      end
+
+      def delete(path:)
+        # S3 cannot delete recursively, files must be individually deleted
+        # Rather than rebuild from the cache simply re-list the path and multi-delete all the objects within
+        list_out = @s3.list_objects_v2(bucket: @bucket, prefix: path.to_s)
+
+        # Simplify for the BackwoodsBackup use case: The cleaner shouldn't delete more than [keys] * 2 files in a shot, so don't bother with pagination
+        # If there is an attempt to delete over 1000 files (the pagination limit) then something is probably wrong.
+        raise(S3CommunicatorError, "Attempt to delete over #{list_out.contents.length} objects in one request") unless list_out.next_continuation_token.nil?
+
+        if list_out.contents.empty?
+          # Missing file, attempting to delete the directory
+          raise(S3CommunicatorError, "Cache out of sync for #{path}") if @cache.exists?(path: path)
+
+          return
+        end
+
+        @s3.delete_objects(bucket: @bucket, delete: { objects: list_out.contents.map { |c| { key: c.key } } })
+        @cache.delete(path: path)
+      end
+
+      def download(path:)
+        get_response = @s3.get_object(bucket: @bucket, key: path.to_s)
+        raise(S3CommunicatorError, "Object length mismatch for #{path}: #{get_response.content_length}:#{get_response.body.length}") if get_response.content_length != get_response.body.length
+
+        return get_response.body.read
+      end
+
+      def exists?(path:)
+        return true if @cache.exists?(path: path)
+
+        _s3_list(path: path)
+        return @cache.exists?(path: path)
+      end
+
+      def list(path:)
+        # Seed the cache, if needed
+        _s3_list(path: path) unless @cache.complete?(path: path)
+
+        @cache.children(path: path).sort.map { |child| path.join(child) }
+      end
+
+      def upload(path:, payload:)
+        @s3.put_object(bucket: @bucket, key: path.to_s, body: payload, storage_class: @storage_class)
+        @cache.add(path: path, date: Time.now)
+      end
+
+      private
+
+      def _cache_add_s3_list_output(contents:)
+        contents.each do |object|
+          @cache.add(path: object.key, date: object.last_modified)
+        end
+      end
+
+      def _s3_list(path:)
+        # This is a cost optimization for the BackwoodsBackup use case.
+        # AWS charges per request, so it's cost effective to cast as wide a net as possible and store the results in memory
+        # (hence the S3ListCache class)
+        # The block shas (2nd level) are too unique, optimizing for them is past the point of diminishing returns
+        # However everything within a sha are practical to list in one request
+        # This will save cost on key lookups.
+        path_array = BackupEngine::Pathname.new(path).to_a.map(&:to_s)
+        raise("Error optimizing #{path}: [0] is not '.'") unless path_array[0] == '.'
+
+        list_path = path_array[1..2].join('/')
+        list_out = @s3.list_objects_v2(bucket: @bucket, prefix: list_path)
+        _cache_add_s3_list_output(contents: list_out.contents)
+        until list_out.next_continuation_token.nil?
+          list_out = @s3.list_objects_v2(bucket: @bucket, continuation_token: list_out.next_continuation_token)
+          _cache_add_s3_list_output(contents: list_out.contents)
+        end
+
+        @cache.mark_complete(path: path)
+      end
     end
   end
 end
---
+++ app/spec/unit/backup_engine/communicator_backend/s3_list_cache_spec.rb
@@ -0,0 +1,294 @@
+require_relative '../../../spec_helper.rb'
+
+require_relative '../../../../lib/backup_engine/communicator_backend/s3_list_cache.rb'
+
+describe 'Backup Engine: unit' do
+  describe BackupEngine::CommunicatorBackend::S3ListCache do
+    let(:test_class) { described_class.new }
+
+    describe '[]' do
+      it 'converts the path to an array and calls lookup_by_array' do
+        expect(test_class).to receive(:lookup_by_array).with(path_array: %w[foo bar baz])
+        test_class['foo/bar/baz']
+      end
+
+      it 'rasies exception on absolute paths' do
+        expect { test_class['/foo/bar/baz'] }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+    end
+
+    describe 'add' do
+      it 'converts the path to an array and calls add_by_array' do
+        expect(test_class).to receive(:add_by_array).with(path_array: %w[foo bar baz], date: 'test_date')
+        test_class.add(path: 'foo/bar/baz', date: 'test_date')
+      end
+
+      it 'rasies exception on absolute paths' do
+        expect { test_class.add(path: '/foo/bar/baz', date: 'test_date') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+    end
+
+    describe 'add_by_array' do
+      it 'adds to the cache' do
+        test_class.add_by_array(path_array: %w[foo bar], date: 100)
+        test_class.add_by_array(path_array: %w[foo baz], date: 200)
+        test_class.add_by_array(path_array: %w[bar baz], date: 300)
+
+        expect(test_class.cache['foo'].cache['bar'].date).to eql(100)
+        expect(test_class.cache['foo'].cache['baz'].date).to eql(200)
+        expect(test_class.cache['bar'].cache['baz'].date).to eql(300)
+      end
+
+      it 'applies the date to all lower keys' do
+        test_class.add_by_array(path_array: %w[foo bar baz], date: 100)
+        expect(test_class.cache['foo'].cache['bar'].cache['baz'].date).to eql(100)
+        expect(test_class.cache['foo'].cache['bar'].date).to eql(100)
+        expect(test_class.cache['foo'].date).to eql(100)
+      end
+
+      it 'updates lower keys without discarding data' do
+        test_class.add_by_array(path_array: %w[foo bar], date: 100)
+        test_class.add_by_array(path_array: %w[foo baz], date: 200)
+        test_class.add_by_array(path_array: %w[foo], date: 300)
+
+        expect(test_class.cache['foo'].cache['bar'].date).to eql(100)
+        expect(test_class.cache['foo'].cache['baz'].date).to eql(200)
+        expect(test_class.cache['foo'].date).to eql(300)
+      end
+
+      it 'propagates the newest date down' do
+        test_class.add_by_array(path_array: %w[foo bar baz], date: 300)
+        test_class.add_by_array(path_array: %w[foo bar], date: 200)
+        test_class.add_by_array(path_array: %w[foo], date: 100)
+
+        expect(test_class.cache['foo'].cache['bar'].cache['baz'].date).to eql(300)
+        expect(test_class.cache['foo'].cache['bar'].date).to eql(300)
+        expect(test_class.cache['foo'].date).to eql(300)
+      end
+    end
+
+    describe 'cache' do
+      it 'is immutable' do
+        expect { test_class.cache['foo'] = nil }.to raise_exception(FrozenError)
+      end
+    end
+
+    describe 'children' do
+      it 'converts the path to an array and calls children_by_array' do
+        expect(test_class).to receive(:children_by_array).with(path_array: %w[foo bar baz])
+        test_class.children(path: 'foo/bar/baz')
+      end
+
+      it 'rasies exception on absolute paths' do
+        expect { test_class.children(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+    end
+
+    describe 'children_by_array' do
+      it 'raises exception on incomplete cache' do
+        test_class.add_by_array(path_array: %w[foo], date: 100)
+        expect { test_class.children_by_array(path_array: %w[foo]) }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+
+      it 'returns the child list on empty list' do
+        test_class.add_by_array(path_array: %w[foo], date: 100)
+        test_class.add_by_array(path_array: %w[bar], date: 300)
+        test_class.add_by_array(path_array: %w[baz], date: 200)
+        test_class.mark_complete_by_array(path_array: [])
+
+        expect(test_class.children_by_array(path_array: [])).to eq %w[foo bar baz]
+      end
+
+      it 'raises exception unknown key' do
+        test_class.mark_complete_by_array(path_array: [])
+        expect { test_class.children_by_array(path_array: %w[unknown key]) }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+
+      it 'returns nested child lists' do
+        test_class.add_by_array(path_array: %w[foo bar baz foobar], date: 100)
+        test_class.add_by_array(path_array: %w[foo bar baz foobaz], date: 100)
+        test_class.add_by_array(path_array: %w[bar], date: 300)
+        test_class.add_by_array(path_array: %w[baz], date: 200)
+        test_class.mark_complete_by_array(path_array: [])
+
+        expect(test_class.children_by_array(path_array: %w[foo bar baz])).to eq %w[foobar foobaz]
+      end
+    end
+
+    describe 'complete?' do
+      it 'defaults to false' do
+        expect(test_class.complete?).to be false
+      end
+
+      it 'returns self complete flag on no path' do
+        test_class.mark_complete_by_array(path_array: [])
+        expect(test_class.complete?).to be true
+      end
+
+      it 'converts the path to an array and calls complete_by_array' do
+        expect(test_class).to receive(:complete_by_array?).with(path_array: %w[foo bar baz])
+        test_class.complete?(path: 'foo/bar/baz')
+      end
+
+      it 'rasies exception on absolute paths' do
+        expect { test_class.complete?(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+    end
+
+    describe 'complete_by_array?' do
+      it 'defaults to false' do
+        expect(test_class.complete_by_array?(path_array: [])).to be false
+      end
+
+      it 'returns false on unknown key' do
+        test_class.mark_complete_by_array(path_array: [])
+        expect(test_class.complete_by_array?(path_array: [])).to be true
+        expect(test_class.complete_by_array?(path_array: %w[unknown key])).to be false
+      end
+
+      it 'returns true when marked complete' do
+        test_class.mark_complete_by_array(path_array: [])
+        expect(test_class.complete_by_array?(path_array: [])).to be true
+      end
+
+      it 'returns complete for child caches' do
+        test_class.add_by_array(path_array: %w[foo bar], date: 100)
+        test_class.mark_complete_by_array(path_array: %w[foo bar])
+
+        expect(test_class.complete_by_array?(path_array: [])).to be false
+        expect(test_class.complete_by_array?(path_array: %w[foo])).to be false
+        expect(test_class.complete_by_array?(path_array: %w[foo bar])).to be true
+      end
+    end
+
+    describe 'delete' do
+      it 'converts the path to an array and calls delete_by_array' do
+        expect(test_class).to receive(:delete_by_array).with(path_array: %w[foo bar baz])
+        test_class.delete(path: 'foo/bar/baz')
+      end
+
+      it 'rasies exception on absolute paths' do
+        expect { test_class.delete(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+    end
+
+    describe 'delete_by_array' do
+      it 'empties the cache on empty list' do
+        test_class.add_by_array(path_array: %w[foo bar baz foobar], date: 100)
+        test_class.delete_by_array(path_array: [])
+        expect(test_class.cache).to be_empty
+      end
+
+      it 'raises exception unknown key' do
+        expect { test_class.delete_by_array(path_array: %w[unknown key]) }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+
+      it 'recursively deletes child lists and empty parents' do
+        test_class.add_by_array(path_array: %w[foo bar baz foobar], date: 100)
+        test_class.add_by_array(path_array: %w[foo baz foobar], date: 200)
+
+        test_class.delete_by_array(path_array: %w[foo bar baz])
+        expect(test_class.cache['foo'].cache.keys).to eql %w[baz]
+      end
+    end
+
+    describe 'exists?' do
+      it 'converts the path to an array and calls exists_by_array?' do
+        expect(test_class).to receive(:exists_by_array?).with(path_array: %w[foo bar baz])
+        test_class.exists?(path: 'foo/bar/baz')
+      end
+
+      it 'rasies exception on absolute paths' do
+        expect { test_class.exists?(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+    end
+
+    describe 'exists_by_array?' do
+      it 'returns true on empty list' do
+        expect(test_class.exists_by_array?(path_array: [])).to be true
+      end
+
+      it 'returns false on unknown key' do
+        expect(test_class.exists_by_array?(path_array: %w[unknown key])).to be false
+      end
+
+      it 'returns true for known children' do
+        test_class.add_by_array(path_array: %w[foo bar], date: 100)
+        test_class.add_by_array(path_array: %w[foo baz], date: 200)
+        test_class.add_by_array(path_array: %w[bar baz], date: 300)
+
+        expect(test_class.exists_by_array?(path_array: %w[foo bar])).to be true
+        expect(test_class.exists_by_array?(path_array: %w[foo baz])).to be true
+        expect(test_class.exists_by_array?(path_array: %w[bar baz])).to be true
+      end
+    end
+
+    describe 'lookup_by_array' do
+      it 'returns the parent date on empty list' do
+        raise('Test Error: test_class date not in expected state') unless test_class.date == 0
+
+        expect(test_class.lookup_by_array(path_array: [])).to eq 0
+      end
+
+      it 'returns the parent date on unknown key' do
+        raise('Test Error: test_class date not in expected state') unless test_class.date == 0
+
+        expect(test_class.lookup_by_array(path_array: %w[unknown key])).to eq 0
+      end
+
+      it 'returns child dates' do
+        test_class.add_by_array(path_array: %w[foo bar], date: 100)
+        test_class.add_by_array(path_array: %w[foo baz], date: 200)
+        test_class.add_by_array(path_array: %w[bar baz], date: 300)
+
+        expect(test_class.lookup_by_array(path_array: %w[foo bar])).to eql(100)
+        expect(test_class.lookup_by_array(path_array: %w[foo baz])).to eql(200)
+        expect(test_class.lookup_by_array(path_array: %w[bar baz])).to eql(300)
+      end
+    end
+
+    describe 'mark_complete' do
+      it 'converts the path to an array and calls mark_complete_by_array' do
+        expect(test_class).to receive(:mark_complete_by_array).with(path_array: %w[foo bar baz])
+        test_class.mark_complete(path: 'foo/bar/baz')
+      end
+
+      it 'rasies exception on absolute paths' do
+        expect { test_class.mark_complete(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+    end
+
+    describe 'mark_complete_by_array' do
+      it 'marks all self and all child caches complete on empty array' do
+        test_class.add_by_array(path_array: %w[foo bar baz], date: 100)
+        test_class.mark_complete_by_array(path_array: [])
+
+        expect(test_class.complete?).to be true
+        expect(test_class.cache['foo'].complete?).to be true
+        expect(test_class.cache['foo'].cache['bar'].complete?).to be true
+      end
+
+      it 'no-ops on unknown key' do
+        test_class.mark_complete_by_array(path_array: %w[unknown key])
+        expect(test_class.complete?).to be false
+      end
+
+      it 'recursively marks complete child lists but not parents' do
+        test_class.add_by_array(path_array: %w[foo bar baz foobar], date: 100)
+        test_class.add_by_array(path_array: %w[foo bar foobar], date: 200)
+        test_class.add_by_array(path_array: %w[foo baz foobar], date: 300)
+
+        test_class.mark_complete_by_array(path_array: %w[foo bar])
+        expect(test_class.complete?).to be false
+        expect(test_class.cache['foo'].complete?).to be false
+        expect(test_class.cache['foo'].cache['baz'].complete?).to be false
+        expect(test_class.cache['foo'].cache['baz'].cache['foobar'].complete?).to be false
+
+        expect(test_class.cache['foo'].cache['bar'].complete?).to be true
+        expect(test_class.cache['foo'].cache['bar'].cache['baz'].complete?).to be true
+        expect(test_class.cache['foo'].cache['bar'].cache['baz'].cache['foobar'].complete?).to be true
+        expect(test_class.cache['foo'].cache['bar'].cache['foobar'].complete?).to be true
+      end
+    end
+  end
+end
