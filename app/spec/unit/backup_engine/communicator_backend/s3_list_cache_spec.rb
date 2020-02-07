c26afe1f4ce2e8194a15c672c917b22d72deeffc
Tom Noonan II <tom@tjnii.com>
2020-02-06 22:32:47 -0500

    [S3 Cache Refactor] Refactor _by_array methods out of tests

--- app/spec/unit/backup_engine/communicator_backend/s3_list_cache_spec.rb
+++ app/spec/unit/backup_engine/communicator_backend/s3_list_cache_spec.rb
@@ -7,32 +7,42 @@ describe 'Backup Engine: unit' do
     let(:test_class) { described_class.new }
 
     describe '[]' do
-      it 'converts the path to an array and calls lookup_by_array' do
-        expect(test_class).to receive(:lookup_by_array).with(path_array: %w[foo bar baz])
-        test_class['foo/bar/baz']
-      end
-
       it 'rasies exception on absolute paths' do
         expect { test_class['/foo/bar/baz'] }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
+      end
+
+      it 'returns the parent date on empty key' do
+        raise('Test Error: test_class date not in expected state') unless test_class.date == 0
+
+        expect(test_class['']).to eq 0
+      end
+
+      it 'returns the parent date on unknown key' do
+        raise('Test Error: test_class date not in expected state') unless test_class.date == 0
+
+        expect(test_class['unknown/key']).to eq 0
+      end
+
+      it 'returns child dates' do
+        test_class.add(path: 'foo/bar', date: 100)
+        test_class.add(path: 'foo/baz', date: 200)
+        test_class.add(path: 'bar/baz', date: 300)
+
+        expect(test_class['foo/bar']).to eql(100)
+        expect(test_class['foo/baz']).to eql(200)
+        expect(test_class['bar/baz']).to eql(300)
       end
     end
 
     describe 'add' do
-      it 'converts the path to an array and calls add_by_array' do
-        expect(test_class).to receive(:add_by_array).with(path_array: %w[foo bar baz], date: 'test_date')
-        test_class.add(path: 'foo/bar/baz', date: 'test_date')
-      end
-
       it 'rasies exception on absolute paths' do
         expect { test_class.add(path: '/foo/bar/baz', date: 'test_date') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
       end
-    end
 
-    describe 'add_by_array' do
       it 'adds to the cache' do
-        test_class.add_by_array(path_array: %w[foo bar], date: 100)
-        test_class.add_by_array(path_array: %w[foo baz], date: 200)
-        test_class.add_by_array(path_array: %w[bar baz], date: 300)
+        test_class.add(path: 'foo/bar', date: 100)
+        test_class.add(path: 'foo/baz', date: 200)
+        test_class.add(path: 'bar/baz', date: 300)
 
         expect(test_class.cache['foo'].cache['bar'].date).to eql(100)
         expect(test_class.cache['foo'].cache['baz'].date).to eql(200)
@@ -40,16 +50,16 @@ describe 'Backup Engine: unit' do
       end
 
       it 'applies the date to all lower keys' do
-        test_class.add_by_array(path_array: %w[foo bar baz], date: 100)
+        test_class.add(path: 'foo/bar/baz', date: 100)
         expect(test_class.cache['foo'].cache['bar'].cache['baz'].date).to eql(100)
         expect(test_class.cache['foo'].cache['bar'].date).to eql(100)
         expect(test_class.cache['foo'].date).to eql(100)
       end
 
       it 'updates lower keys without discarding data' do
-        test_class.add_by_array(path_array: %w[foo bar], date: 100)
-        test_class.add_by_array(path_array: %w[foo baz], date: 200)
-        test_class.add_by_array(path_array: %w[foo], date: 300)
+        test_class.add(path: 'foo/bar', date: 100)
+        test_class.add(path: 'foo/baz', date: 200)
+        test_class.add(path: 'foo', date: 300)
 
         expect(test_class.cache['foo'].cache['bar'].date).to eql(100)
         expect(test_class.cache['foo'].cache['baz'].date).to eql(200)
@@ -57,9 +67,9 @@ describe 'Backup Engine: unit' do
       end
 
       it 'propagates the newest date down' do
-        test_class.add_by_array(path_array: %w[foo bar baz], date: 300)
-        test_class.add_by_array(path_array: %w[foo bar], date: 200)
-        test_class.add_by_array(path_array: %w[foo], date: 100)
+        test_class.add(path: 'foo/bar/baz', date: 300)
+        test_class.add(path: 'foo/bar', date: 200)
+        test_class.add(path: 'foo', date: 100)
 
         expect(test_class.cache['foo'].cache['bar'].cache['baz'].date).to eql(300)
         expect(test_class.cache['foo'].cache['bar'].date).to eql(300)
@@ -74,122 +84,78 @@ describe 'Backup Engine: unit' do
     end
 
     describe 'children' do
-      it 'converts the path to an array and calls children_by_array' do
-        expect(test_class).to receive(:children_by_array).with(path_array: %w[foo bar baz])
-        test_class.children(path: 'foo/bar/baz')
-      end
-
       it 'rasies exception on absolute paths' do
         expect { test_class.children(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
       end
-    end
 
-    describe 'children_by_array' do
-      it 'returns the child list on empty list' do
-        test_class.add_by_array(path_array: %w[foo], date: 100)
-        test_class.add_by_array(path_array: %w[bar], date: 300)
-        test_class.add_by_array(path_array: %w[baz], date: 200)
+      it 'returns the child list on empty path' do
+        test_class.add(path: 'foo', date: 100)
+        test_class.add(path: 'bar', date: 300)
+        test_class.add(path: 'baz', date: 200)
 
-        expect(test_class.children_by_array(path_array: [])).to eq %w[foo bar baz]
+        expect(test_class.children(path: '.')).to eq %w[foo bar baz]
       end
 
       it 'raises exception on unknown key' do
-        expect { test_class.children_by_array(path_array: %w[unknown key]) }.to raise_exception(Errno::ENOENT)
+        expect { test_class.children(path: 'unknown/key') }.to raise_exception(Errno::ENOENT)
       end
 
       it 'returns nested child lists' do
-        test_class.add_by_array(path_array: %w[foo bar baz foobar], date: 100)
-        test_class.add_by_array(path_array: %w[foo bar baz foobaz], date: 100)
-        test_class.add_by_array(path_array: %w[bar], date: 300)
-        test_class.add_by_array(path_array: %w[baz], date: 200)
+        test_class.add(path: 'foo/bar/baz/foobar', date: 100)
+        test_class.add(path: 'foo/bar/baz/foobaz', date: 100)
+        test_class.add(path: 'bar', date: 300)
+        test_class.add(path: 'baz', date: 200)
 
-        expect(test_class.children_by_array(path_array: %w[foo bar baz])).to eq %w[foobar foobaz]
+        expect(test_class.children(path: 'foo/bar/baz')).to eq %w[foobar foobaz]
       end
     end
 
     describe 'delete' do
-      it 'converts the path to an array and calls delete_by_array' do
-        expect(test_class).to receive(:delete_by_array).with(path_array: %w[foo bar baz])
-        test_class.delete(path: 'foo/bar/baz')
-      end
-
       it 'rasies exception on absolute paths' do
         expect { test_class.delete(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
       end
-    end
 
-    describe 'delete_by_array' do
-      it 'empties the cache on empty list' do
-        test_class.add_by_array(path_array: %w[foo bar baz foobar], date: 100)
-        test_class.delete_by_array(path_array: [])
+      it 'empties the cache on empty path' do
+        test_class.add(path: 'foo/bar/baz/foobar', date: 100)
+        test_class.delete(path: '.')
         expect(test_class.cache).to be_empty
       end
 
       it 'raises exception unknown key' do
-        expect { test_class.delete_by_array(path_array: %w[unknown key]) }.to raise_exception(Errno::ENOENT)
+        expect { test_class.delete(path: 'unknown/key') }.to raise_exception(Errno::ENOENT)
       end
 
       it 'recursively deletes child lists and empty parents' do
-        test_class.add_by_array(path_array: %w[foo bar baz foobar], date: 100)
-        test_class.add_by_array(path_array: %w[foo baz foobar], date: 200)
+        test_class.add(path: 'foo/bar/baz/foobar', date: 100)
+        test_class.add(path: 'foo/baz/foobar', date: 200)
 
-        test_class.delete_by_array(path_array: %w[foo bar baz])
+        test_class.delete(path: 'foo/bar/baz')
         expect(test_class.cache['foo'].cache.keys).to eql %w[baz]
       end
     end
 
     describe 'exists?' do
-      it 'converts the path to an array and calls exists_by_array?' do
-        expect(test_class).to receive(:exists_by_array?).with(path_array: %w[foo bar baz])
-        test_class.exists?(path: 'foo/bar/baz')
-      end
-
       it 'rasies exception on absolute paths' do
         expect { test_class.exists?(path: '/foo/bar/baz') }.to raise_exception(BackupEngine::CommunicatorBackend::S3ListCacheError)
       end
-    end
 
-    describe 'exists_by_array?' do
-      it 'returns true on empty list' do
-        expect(test_class.exists_by_array?(path_array: [])).to be true
+      # TODO: _by_array artefact
+      it 'returns true on empty path' do
+        expect(test_class.exists?(path: '.')).to be true
       end
 
       it 'returns false on unknown key' do
-        expect(test_class.exists_by_array?(path_array: %w[unknown key])).to be false
+        expect(test_class.exists?(path: 'unknown/key')).to be false
       end
 
       it 'returns true for known children' do
-        test_class.add_by_array(path_array: %w[foo bar], date: 100)
-        test_class.add_by_array(path_array: %w[foo baz], date: 200)
-        test_class.add_by_array(path_array: %w[bar baz], date: 300)
+        test_class.add(path: 'foo/bar', date: 100)
+        test_class.add(path: 'foo/baz', date: 200)
+        test_class.add(path: 'bar/baz', date: 300)
 
-        expect(test_class.exists_by_array?(path_array: %w[foo bar])).to be true
-        expect(test_class.exists_by_array?(path_array: %w[foo baz])).to be true
-        expect(test_class.exists_by_array?(path_array: %w[bar baz])).to be true
-      end
-    end
-
-    describe 'lookup_by_array' do
-      it 'returns the parent date on empty list' do
-        raise('Test Error: test_class date not in expected state') unless test_class.date == 0
-
-        expect(test_class.lookup_by_array(path_array: [])).to eq 0
-      end
-
-      it 'returns the parent date on unknown key' do
-        raise('Test Error: test_class date not in expected state') unless test_class.date == 0
-
-        expect(test_class.lookup_by_array(path_array: %w[unknown key])).to eq 0
-      end
-
-      it 'returns child dates' do
-        test_class.add_by_array(path_array: %w[foo bar], date: 100)
-        test_class.add_by_array(path_array: %w[foo baz], date: 200)
-        test_class.add_by_array(path_array: %w[bar baz], date: 300)
-
-        expect(test_class.lookup_by_array(path_array: %w[foo bar])).to eql(100)
-        expect(test_class.lookup_by_array(path_array: %w[foo baz])).to eql(200)
-        expect(test_class.lookup_by_array(path_array: %w[bar baz])).to eql(300)
+        expect(test_class.exists?(path: 'foo/bar')).to be true
+        expect(test_class.exists?(path: 'foo/baz')).to be true
+        expect(test_class.exists?(path: 'bar/baz')).to be true
       end
     end
   end
